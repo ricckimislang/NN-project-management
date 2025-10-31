4859fd296ea96d6e5eec6ceaac60f7bb8d30c8cf
riccki mislang <dragonmislang9@gmail.com>
2025-10-31 15:17:52 +0800

    added shadcn button

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,6 +2,7 @@
 import AppMain from '@/components/layout/AppMain.vue'
 import ProjectCard from '@/components/project_module/ProjectCard.vue'
 import BreadCrumbs from '@/components/layout/BreadCrumbs.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
@@ -11,21 +12,15 @@ import BreadCrumbs from '@/components/layout/BreadCrumbs.vue'
         <BreadCrumbs :url="'/projects'" title="Projects"></BreadCrumbs>
       </div>
       <div class="flex space-x-2">
-        <button
-          class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded flex items-center"
-        >
+        <Button variant="default" size="default">
           <i class="fa-solid fa-plus"></i> Add Project
-        </button>
-        <button
-          class="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 rounded flex items-center"
-        >
+        </Button>
+        <Button variant="outline" size="default">
           <i class="fa-solid fa-magnifying-glass"></i> Search
-        </button>
-        <button
-          class="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 rounded flex items-center"
-        >
+        </Button>
+        <Button variant="outline" size="default">
           <i class="fa-solid fa-gear"></i> Filter / Sort
-        </button>
+        </Button>
       </div>
     </div>
     <ProjectCard />
